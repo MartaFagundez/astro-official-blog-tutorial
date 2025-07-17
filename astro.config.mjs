2584f3fd52a86a9fb524fbb491fdbd8942ceafc1
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2025-07-17 18:09:18 -0300

    feat: add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://my-astro-official-blog-tutorial.netlify.app"
+});
