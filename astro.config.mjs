91fa96ccaf8f7c3363a60229c343c742eb8a56a8
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2025-07-17 18:14:46 -0300

    build: install Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://my-astro-official-blog-tutorial.netlify.app"
+  site: "https://my-astro-official-blog-tutorial.netlify.app",
+  integrations: [preact()]
 });
